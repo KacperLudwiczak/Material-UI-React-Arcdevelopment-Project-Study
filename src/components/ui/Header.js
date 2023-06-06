cd884f04f9590a9cfb8cf05516df3bc39d37c5f9
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-06-06 18:25:32 +0200

    Styling - Inline

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import { AppBar } from "@material-ui/core";
 import { Toolbar } from "@material-ui/core";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -16,16 +17,25 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: { ...theme.mixins.toolbar },
+}));
+
 export default function Header(props) {
+  const classes = useStyles();
+
   return (
-    <ElevationScroll>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <Typography variant="h3" color="secondary">
-            Arc Development
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <>
+      <ElevationScroll>
+        <AppBar position="fixed" color="primary">
+          <Toolbar>
+            <Typography variant="h3" color="secondary">
+              Arc Development
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </>
   );
 }
