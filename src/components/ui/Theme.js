ffb1a0ec6c85327d4b5628e06e19d3b88fdd78cb
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-06-06 17:30:45 +0200

    Styling Themes Palette

---
+++ src/components/ui/Theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const arcBlue = "#0B7289";
+const arcOrange = "#FFBA60";
+
+export default createMuiTheme({
+  palette: {
+    common: { blue: arcBlue, orange: arcOrange },
+    primary: { main: arcBlue },
+    secondary: { main: arcOrange },
+  },
+});
